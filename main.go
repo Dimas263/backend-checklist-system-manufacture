8a871caceb9c0c8ba8c65718e370c5dbb0236365
dwipu <dimas_d_putra@app.co.id>
2025-09-05 19:37:43 +0700

    deploy

--- main.go
+++ main.go
@@ -1,21 +1,22 @@
 package main
 
 import (
+	_ "encoding/json"
 	"net/http"
 
 	"github.com/Dimas263/backend-checklist-system-manufacture/api"
 	"github.com/vercel/go-bridge/go/bridge"
 )
 
+// Middleware CORS
 func withCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		/*w.Header().Set("Access-Control-Allow-Origin", "*")*/
+		// Izinkan domain frontend
 		w.Header().Set("Access-Control-Allow-Origin", "https://frontend-checklist-system-manufactu.vercel.app")
-		/*w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")*/
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		// kalau OPTIONS (preflight), balas kosong
+		// preflight request
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
 			return
@@ -30,5 +31,5 @@ func main() {
 	mux.HandleFunc("/login", withCORS(api.LoginHandler))
 	mux.HandleFunc("/checklist", withCORS(api.ChecklistHandler))
 
-	bridge.Start(mux) // <-- masukkan mux sebagai http.Handler
+	bridge.Start(mux)
 }
