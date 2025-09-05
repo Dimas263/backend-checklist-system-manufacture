68981296bad27389ec0a958d7955100fd44bf5a7
dwipu <dimas_d_putra@app.co.id>
2025-09-05 17:34:16 +0700

    initial commit

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func withCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		/*w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")*/
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
