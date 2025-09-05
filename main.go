9f066e15fa1541c8367a40c5fc8059cfbb0171a3
dwipu <dimas_d_putra@app.co.id>
2025-09-05 18:50:43 +0700

    initial commit

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 
 func withCORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		/*w.Header().Set("Access-Control-Allow-Origin", "*")*/
-		w.Header().Set("Access-Control-Allow-Origin", "https://frontend-checklist-system-manufactu.vercel.app/")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		/*w.Header().Set("Access-Control-Allow-Origin", "https://frontend-checklist-system-manufactu.vercel.app/")*/
 		/*w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")*/
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
